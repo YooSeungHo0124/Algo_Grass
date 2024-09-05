731b05fbe80b62f719245e4a614ea5fc4ddd822f
SeougHo Yoo <tmdgh0124@korea.ac.kr>
2024-09-05 12:42:18 +0900

    [Bronze V] Title: A+B, Time: 172 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    
+
+static public void main(String args[]){
+    Scanner s = new Scanner(System.in);
+    int A = s.nextInt();
+    int B = s.nextInt();
+    System.out.println(A+B);
+}
+   }
